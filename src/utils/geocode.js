25041aa762716568c4154a87d9ebb1716449e05e
ryan erswell <ersie93@googlemail.com>
2021-04-25 20:31:47 +0100

    connect api to app

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -6,7 +6,7 @@ const geocode = (address, callback) => {
 	request({ url, json: true}, (error, {body}) => {
 		if(error){
 			callback('Unable to connect to location services!');
-		} else if(!body.features){
+		} else if(!body.features[0]){
 			callback('Unable to find location!');
 		} else {
 			callback(undefined, {
